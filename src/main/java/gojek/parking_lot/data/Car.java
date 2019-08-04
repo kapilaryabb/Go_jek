6a9d5d76235ae91d5b1fa6c73046a3fbfd9ef039
Kapil <kapil@LAP-C02T74Y4FVH3.local>
2019-08-04 20:20:50 +0530

    added car entity

--- src/main/java/gojek/parking_lot/data/Car.java
+++ src/main/java/gojek/parking_lot/data/Car.java
@@ -1,4 +1,19 @@
 package gojek.parking_lot.data;
 
-public class Car {
+public class Car{
+    private String regNo;
+    private String colour;
+
+    public Car(String r, String c){
+        this.regNo = r;
+        this.colour = c;
+    }
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public String getColour() {
+        return colour;
+    }
 }
